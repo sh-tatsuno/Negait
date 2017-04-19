7685f1c590072fd4a91b13e0c3e9b6c1abcb6bf7
“sh-tatsuno;git config --global user.email “wasabi.mandarin@gmail.com <wasabi.mandarin@gmail.com>
2017-04-19 18:54:49 +0900

    update layout and post

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,7 +2,7 @@ require 'Time'
 class TopController < ApplicationController
 
   def index
-    @posts = Post.order("created_at DESC").page(params[:page]).per(3)
+    @posts = Post.order("created_at DESC").page(params[:page]).per(15)
   end
 
 
