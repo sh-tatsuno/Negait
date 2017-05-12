a975154fba556b49aebb8bc8691cda503c7ba61c
“sh-tatsuno;git config --global user.email “wasabi.mandarin@gmail.com <wasabi.mandarin@gmail.com>
2017-05-12 16:20:44 +0900

    add config

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar,
                       :storage => :cloudinary,
+                      :cloudinary_credentials => Rails.root.join("config/cloudinary.yml"),
                       styles:  { medium: "300x300#", thumb: "100x100#" }
   validates_attachment_content_type :avatar,
                                       content_type: ["image/jpg","image/jpeg","image/png"]
