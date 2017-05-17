992b9a029820375387cc474846113118ac8c6f70
“sh-tatsuno;git config --global user.email “wasabi.mandarin@gmail.com <wasabi.mandarin@gmail.com>
2017-05-17 10:22:23 +0900

    add attribute

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,12 +2,21 @@ class TopController < ApplicationController
 
   #attributeの分岐を作成
   def index
-    if (params[:category].nil? or params[:category]=="all")
-      @posts = Post.where('user_id != :user_id', {user_id: current_user.id}).order("created_at DESC").page(params[:page]).per(15)
+    if (params[:attribute]=="available")
+      if (params[:category].nil? or params[:category]=="all")
+        @posts = Post.where('user_id != :user_id', {user_id: current_user.id}).where('completed = 0 and created_at > :limits', {limits: Time.now - 14 * (60*60*24)}).order("created_at DESC").page(params[:page]).per(15)
+      else
+        @posts = Post.where('user_id != :user_id', {user_id: current_user.id}).where('category LIKE(?)',"#{params[:category]}").where('completed = 0 and created_at > :limits', {limits: Time.now - 14 * (60*60*24)}).order("created_at DESC").page(params[:page]).per(15)
+      end
     else
-      @posts = Post.where('user_id != :user_id', {user_id: current_user.id}).where('category LIKE(?)',"#{params[:category]}").order("created_at DESC").page(params[:page]).per(15)
+      if (params[:category].nil? or params[:category]=="all")
+        @posts = Post.where('user_id != :user_id', {user_id: current_user.id}).order("created_at DESC").page(params[:page]).per(15)
+      else
+        @posts = Post.where('user_id != :user_id', {user_id: current_user.id}).where('category LIKE(?)',"#{params[:category]}").order("created_at DESC").page(params[:page]).per(15)
+      end
     end
   end
 
 
 end
+#where('completed = 0 and created_at < :limits', {limits: Time.now - 14 * (60*60*24)}).
